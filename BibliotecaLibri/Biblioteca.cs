96bf2aacc3044dafaba41ed7c01eda9c2a57a519
alessandro.diego <alessandro.diego@PC7305>
2020-11-18 11:16:36 +0100

    Aggiunta Metodo AddLibro e  RicercaLibro

--- BibliotecaLibri/Biblioteca.cs
+++ BibliotecaLibri/Biblioteca.cs
@@ -8,5 +8,60 @@ namespace BibliotecaLibri
 {
     class Biblioteca
     {
+        public Biblioteca(string nome, string indirizzo, string orarioApertura, string orarioChiusura, List<Libri> listaLibri)
+        {
+            Nome = nome;
+            Indirizzo = indirizzo;
+            ListaLibri = listaLibri;
+            OrarioApertura = orarioApertura;
+            OrarioChiusura = orarioChiusura;
+        }
+
+        public string Nome
+        {
+            get;
+            set;
+        }
+        public string Indirizzo
+        {
+            get;
+            set;
+        }
+
+        public List<Libri> ListaLibri
+        {
+            get;
+            set;
+        }
+
+        public string OrarioApertura
+        {
+            get;
+            set;
+        }
+
+        public string OrarioChiusura
+        {
+            get;
+            set;
+        }
+        public void AddLibro(Libri l)
+        {
+            ListaLibri.Add(l);
+        }
+
+        public Libri RicercaLibro(string titolo)
+        {
+            for (int i = 0; i < ListaLibri.Count; i++)
+            {
+                if (ListaLibri[i].titolo = titolo)
+                {
+                    return ListaLibri[i];
+                }
+            }
+
+            throw new Exception("non c'è il libro cercato");
+
+        }
     }
 }
