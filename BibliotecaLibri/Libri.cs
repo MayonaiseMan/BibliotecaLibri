4f1f62c4f987c358f2e6cd4f5c77eea24778093c
MattiaFilippi <mattia.filippi.stud@ispascalcomandi.it>
2020-11-18 10:46:48 +0100

    Creazione classe Libri

---
+++ BibliotecaLibri/Libri.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BibliotecaLibri
+{
+    public class Libri
+    {
+        public int Autore
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Titolo
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int AnnoPubblicazione
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int Editore
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int NPag
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
